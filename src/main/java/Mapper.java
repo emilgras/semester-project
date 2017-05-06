606f1c56652eacc6e6082bd69f3bab41b41bbe07
Daniel Hillmann <hiller@live.dk>
2017-05-06 10:41:23 +0200

    Initialize unit tests

---
+++ src/main/java/Mapper.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Mapper {
+
+
+    public String[] getAllCitiesByBookTitle() {
+
+        String [] cities = {"city one", "city two", "city three"};
+
+        return cities;
+    }
+}
--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -1,26 +1,22 @@
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.hamcrest.Matcher;
+import org.junit.*;
 import org.junit.jupiter.api.DisplayName;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.*;
 
 
 public class UnitTests {
 
-    public UnitTests() {
-    }
-
-
+    @Ignore("Not implemented yet")
     @Test
     @DisplayName("should return all books by a city name that corresponds to authors that mention in this city")
     public void testGetBooksByCityName () {
@@ -34,9 +30,23 @@ public class UnitTests {
     }
 
     @Test
-    @DisplayName("should return all cities mentioned by book title")
-    public void getAllCitiesByBookTitle () {
+    //@DisplayName("should return all cities mentioned by book title")
+    public void testGetAllCitiesByBookTitle () {
 
+        final Mapper mapper = new Mapper();
+
+        final String bookTitle = "mybook";
+
+        String expectedCityOne = "city one";
+        String expectedCityTwo = "city two";
+        String expectedCityThree = "city three";
+
+        String[] expectedResult = {expectedCityOne, expectedCityTwo, expectedCityThree};
+
+        String[] actualResult = mapper.getAllCitiesByBookTitle();
+
+
+        assertThat(actualResult, arrayContaining(Arrays.asList(equalTo(expectedCityOne), expectedCityTwo)));
 
     }
 }
