38258bb4bd1a11a0ea6f333ccb246e82f107668e
Anders Bjergfelt <abj@dis-play.dk>
2017-05-09 21:49:07 +0200

    Connection to Neo4j from Java to VM!

---
+++ src/test/java/GraphDBTest/DbConnectionTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GraphDBTest;
+
+import Entities.nosql.Author;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ *
+ * @author abj
+ */
+public class DbConnectionTest {
+    
+    public DbConnectionTest() {
+    }
+
+      private static EntityManagerFactory entityManagerFactory;
+
+    @BeforeClass
+    public static void setUpEntityManagerFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("ogm-neo4j_LOCAL");
+    }
+
+    @AfterClass
+    public static void closeEntityManagerFactory() {
+        entityManagerFactory.close();
+    }
+
+    
+    @Test
+    public void canPersistAndLoadAuthor(){
+    
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    entityManager.getTransaction().begin();
+   
+    // create an Author
+    Author bob = new Author( "Bob");
+    entityManager.persist( bob );
+
+    entityManager.getTransaction().commit();
+
+    // get a new EM to make sure data is actually retrieved from the store and not Hibernate's internal cache
+    entityManager.close();
+    entityManager = entityManagerFactory.createEntityManager();
+    
+     // load it back
+    entityManager.getTransaction().begin();
+
+    Author loadedAuthor = entityManager.find( Author.class, bob.getId() );
+    assertNotNull(loadedAuthor);
+    assertThat(loadedAuthor.getAuthorName(), equalTo("Bob"));
+    entityManager.getTransaction().commit();
+
+    entityManager.close();
+    
+    }
+    
+   
+}
