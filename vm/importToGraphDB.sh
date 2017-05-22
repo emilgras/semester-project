8abaadf05dbb86d7bfef6eb9aaed80790494e394
Anders Bjergfelt <abj@dis-play.dk>
2017-05-22 10:31:28 +0200

    Changes to bash script

--- vm/importToGraphDB.sh
+++ vm/importToGraphDB.sh
@@ -1,6 +1,2 @@
-USING PERIODIC COMMIT
-LOAD CSV WITH HEADERS FROM "file:///city_nodes.csv" AS row
-MERGE (:City {id: row.city_id, name: row.city});
-
 bin/neo4j-import --into books.db --id-type string \
                  --nodes:City city_nodes.csv
