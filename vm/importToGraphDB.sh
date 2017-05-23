a5f734317eba466000bbbc6a038d92fbf0b32ccc
Anders Bjergfelt <abj@dis-play.dk>
2017-05-23 11:08:28 +0200

    working on script

--- vm/importToGraphDB.sh
+++ vm/importToGraphDB.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-../bin/neo4j-import --into books.db --id-type string \
-                 --nodes:City city_nodes.csv
+DIR="$( cd "$( dirname "$0" )" && pwd)"
+CSV_DIR="${DIR}/csvFiles"
+sudo mkdir -p "${CSV_DIR}"
+echo "Ready to work."
+
+if [ -z "$(ls -A ${CSV_DIR})" ]; then
+ wget https://github.com/hilleer/semester-project/raw/master/files/city.zip
+ echo "Work, work."
+ unzip city.zip -d csvFiles/
+ rm city.zip
+ echo "Okidoki."
+ neo4j-admin import --into books.db --id-type string \ --nodes:City "${CSV_DIR}/city_nodes.csv"
+ echo "Jobs done"
+else
+ echo "Hmmm?"
+fi
