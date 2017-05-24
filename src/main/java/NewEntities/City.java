24b94fa1a88558935a3ea59fc7f15a6d9553e579
lalelarsen <frederikblarsen@hotmail.com>
2017-05-24 20:20:20 +0200

    New sql mapper and interface. plus the entities

---
+++ src/main/java/NewEntities/City.java
@@ -0,0 +1,52 @@
+package NewEntities;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Frederik
+ */
+public class City {
+    int id;
+    String cityName;
+    float latitude;
+    float longtitude;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(float longtitude) {
+        this.longtitude = longtitude;
+    }
+    
+    
+}
